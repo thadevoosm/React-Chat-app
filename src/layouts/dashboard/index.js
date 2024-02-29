a09584e9640ede98279a4086301b1c5ce3801f99
thadevoos <thadevoosmbiju@gmail.com>
2024-02-29 23:48:31 +0530

    added theme button, color

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,59 @@
 import React, { useState } from "react";
-import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import {useTheme} from "@mui/material/styles";
+import {useTheme,styled} from "@mui/material/styles";
 import Logo  from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 32,
+    height: 32,
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 20 / 2,
+  },
+}));
 
 const DashboardLayout = () => {
 
@@ -13,6 +61,7 @@ const DashboardLayout = () => {
 
   const [selected, setSelected] = useState(0);
   console.log(theme);
+  const {onToggleMode} = useSettings();
 
   return (
     <>
@@ -24,13 +73,15 @@ const DashboardLayout = () => {
             boxShadow: "0px 0px 2px rgb(0,0,0,0.25)",
             width: 100, 
             height: '100vh',
-            overflowX:'hidden',
           }
         }
       >
-        <Stack direction= "column" alignItems={"center"} spacing={3} justifyContent={"space-between"}
+        <Stack direction= "column" 
+        alignItems={"center"} 
+        spacing={3} 
+        justifyContent={"space-between"}
          sx ={{
-          height:'100%',
+          height: '100%',
          }}
          >
           <Stack alignItems={"center"} spacing={4}>
@@ -61,7 +112,7 @@ const DashboardLayout = () => {
         :<IconButton onClick={()=>{
             setSelected(ka.index)
         }}
-         sx={{width: "max-content", color: 'black',}} key={ka.index}>{ka.icon}</IconButton>
+         sx={{width: "max-content", color: theme.palette.mode ==="Light"?'White':theme.palette.text.primary,}} key={ka.index}>{ka.icon}</IconButton>
         ))}
       </Stack>
 
@@ -79,15 +130,25 @@ const DashboardLayout = () => {
        </Box>
        :<IconButton  onClick={()=>{
         setSelected(3);
-       }} sx={{width: "max-content", color: 'black',}}>
+       }} sx={{width: "max-content", color: theme.palette.mode ==="Light"?'White':theme.palette.text.primary,}}>
         <Gear />
       </IconButton>
       }
       </Stack>
-      </Stack>
-      <Stack >
-        <Avatar src={faker.image.avatar()} />
+      
+      <Stack spacing={4}
+      sx={{
+        alignItems:'center',
+        
+      }}>
+        {}
+        <MaterialUISwitch onChange={()=>{
+          onToggleMode();
+        }} defaultChecked />
+        <Avatar sx={{
+        }} src={faker.image.avatar()} />
       </Stack >
+      </Stack>
       </Box> 
       <Outlet/>
     </>
